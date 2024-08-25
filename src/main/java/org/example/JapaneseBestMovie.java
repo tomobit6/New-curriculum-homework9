94cd46c2e9f934c0182e27e78edc93026dc8ca06
tomobit6 <kotenkotenko6@gamil.com>
2024-08-25 16:05:44 +0900

    JananeseMovieの命名規則に合わせて、1つのインスタンスが１つのmovieを指すように変更

--- src/main/java/org/example/JapaneseBestMovie.java
+++ src/main/java/org/example/JapaneseBestMovie.java
@@ -1,6 +1,7 @@
 package org.example;
 
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class JapaneseBestMovie extends JapaneseMovie {
@@ -8,8 +9,18 @@ public class JapaneseBestMovie extends JapaneseMovie {
   String maxKey = null;
   Double maxValue = 0.0;
 
+  public JapaneseBestMovie(String movieName, Double movieReviewRating) {
+    super(movieName, movieReviewRating);
+  }
+
   public Map<String, Double> reviewMap() {
-    for (Map.Entry<String, Double> entry : super.movieReviewMap.entrySet()) {
+    Map<String, Double> movieReviewMap = new HashMap<>();
+    movieReviewMap.put(movie1.movieName, movie1.movieReviewRating);
+    movieReviewMap.put(movie2.movieName, movie2.movieReviewRating);
+    movieReviewMap.put(movie3.movieName, movie3.movieReviewRating);
+    movieReviewMap.put(movie4.movieName, movie4.movieReviewRating);
+    movieReviewMap.put(movie5.movieName, movie5.movieReviewRating);
+    for (Map.Entry<String, Double> entry : movieReviewMap.entrySet()) {
       if (entry.getValue() > maxValue) {
         maxKey = entry.getKey();
         maxValue = entry.getValue();
